4cf96a339f9105d97e6d9b161d6b25d8cf85b448
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2024-05-21 14:54:19 -0300

    app ready

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ function App() {
         onChange={(e) => setText(e.target.value)}
       />
       <p className={styles.lucida}>{text} (Fuente: Lucida CALLIGRAPHY)</p>
+      <p className={styles.segoe}>{text} (Fuente: Segoe SCRIPT)</p>
+      <p className={styles.monotype}>{text} (Fuente: Monotype CORSIVA)</p>
+      <p className={styles.kghappy}>{text} (Fuente: KG Happy)</p>
+      <p className={styles.msreference}>{text} (Fuente: MS Reference Sans)</p>
+      <p className={styles.itc}>{text} (Fuente: ITC Avant Garde Gothic)</p>
+      <p className={styles.palatino}>{text} (Fuente: PALATINO Linotype)</p>
+      <p className={styles.mvboli}>{text} (Fuente: MV Boli)</p>
+      <p className={styles.impact}>{text} (Fuente: IMPACT)</p>
+      <p className={styles.javanese}>{text} (Fuente: Javanese TEXT)</p>
     </div>
   );
 }
