7cdc856f101ad45d277c7ab98587671c4376364f
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2024-05-21 14:03:50 -0300

    first font ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import styles from "./styles.module.css";
 function App() {
+  const [text, setText] = useState("Lorem ipsum dolor sit amet.");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.appContainer}>
+      <h1>Title Generator</h1>
+      <input
+        placeholder="Ingrese texto a generar"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <p className={styles.lucida}>{text} (Fuente: Lucida CALLIGRAPHY)</p>
     </div>
   );
 }
